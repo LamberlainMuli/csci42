148f47b6397cd1cf03d03343a6b8897c27e31595
saxypingu <maria.naciongayo@student.ateneo.edu>
2025-03-09 21:37:12 +0800

    Made basic CRUD views for Product, started on templates

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, ProductImage
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'seller', 'price', 'category', 'condition', 'is_sold', 'created_at')
+    list_filter = ('category', 'condition', 'is_sold', 'created_at')
+    search_fields = ('title', 'description', 'seller__username')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('product', 'is_primary')
+    list_filter = ('is_primary',)
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from .models import Product
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import *
 
 class HomePage(ListView):
     model = Product
@@ -8,3 +8,38 @@ class HomePage(ListView):
     context_object_name = 'products'
     ordering = ['-created_at']
     paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+
+        for product in queryset:
+            product.primary_image = product.images.filter(is_primary=True).first()
+        if query:
+            queryset = queryset.filter(title__icontains=query) 
+        return queryset
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'marketplace/product_detail.html'
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'marketplace/product_form.html'
+    fields = ['title', 'description', 'price', 'size', 'color', 'material', 'category', 'condition', 'is_sold']
+    
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'marketplace/product_form.html'
+    fields = ['title', 'description', 'price', 'size', 'color', 'material', 'category', 'condition', 'is_sold']
+    
+    def get_queryset(self):
+        return Product.objects.filter(seller=self.request.user)
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'marketplace/product_confirm_delete.html'
+    success_url = reverse_lazy('product-list')
+    
+    def get_queryset(self):
+        return Product.objects.filter(seller=self.request.user)
