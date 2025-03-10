ab873583b6310da6b6b2cabac7946529b105b9a5
silvallana <roxannesilvallana@gmail.com>
2025-03-10 23:18:33 +0800

    Created a custom User model and CustomUserManager

--- user/models.py
+++ user/models.py
@@ -1,3 +1,51 @@
+import uuid
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import (
+    AbstractUser,
+    BaseUserManager,
+	PermissionsMixin,
+)
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Please specify a valid email.")
+        email = self.normalize_email(email) # Normalize email (lowercase)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractUser, PermissionsMixin):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255, unique=True)
+	
+    ROLE_CHOICES = [
+        ('buyer', 'Buyer'),
+        ('seller', 'Seller'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='buyer')
+    
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    objects = CustomUserManager()
+    
+    USERNAME_FIELD = 'email' # Email for auth
+    REQUIRED_FIELDS = ['username'] # Require username when creating user
+    
+    def __str__(self):
+        return self.email
