18db61f6146312d9811ecc9c49191d0a12a415ec
b3LL3 <kyla.apolinario@student.ateneo.edu>
2025-03-12 09:11:03 +0800

    Filled in create and update for product, also added displaying the user role sa profile page.

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePage, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
+from .views import *
 
 app_name = 'marketplace'  # This registers the namespace
 
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,7 @@
 from .models import Product
 from django.urls import reverse_lazy
 from django.views.generic import *
+from django.contrib.auth.mixins import *
 
 class HomePage(ListView):
     model = Product
@@ -23,18 +24,29 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'marketplace/product_detail.html'
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
-    template_name = 'marketplace/product_form.html'
+    template_name = 'marketplace/product_create.html'
+    
     fields = ['title', 'description', 'price', 'size', 'color', 'material', 'category', 'condition', 'is_sold']
+
+    def form_valid(self, form):
+        form.instance.seller = self.request.user 
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('marketplace:product-detail', kwargs={'pk': self.object.pk})
     
 class ProductUpdateView(UpdateView):
     model = Product
-    template_name = 'marketplace/product_form.html'
+    template_name = 'marketplace/product_update.html'
     fields = ['title', 'description', 'price', 'size', 'color', 'material', 'category', 'condition', 'is_sold']
     
     def get_queryset(self):
         return Product.objects.filter(seller=self.request.user)
+    
+    def get_success_url(self):
+        return reverse_lazy('marketplace:home')
 
 class ProductDeleteView(DeleteView):
     model = Product
